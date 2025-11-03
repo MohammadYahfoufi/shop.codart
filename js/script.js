361f4916acfdc35a63d82b0bb595a0b5179e3fe1
MohammadYahfoufi <mhmdyahfoufi52@gmail.com>
2025-11-04 00:36:32 +0200

    autoplay hero card

--- js/script.js
+++ js/script.js
@@ -25,6 +25,11 @@
 
     var swiper = new Swiper(".main-swiper", {
       speed: 500,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      loop: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
